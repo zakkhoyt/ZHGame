f641cea517a8398e81ae7912a618fac52edb8390
Zakk Hoyt <vaporwarewolf@gmail.com>
2016-01-07 20:25:49 -0800

    now that two way communication is working (mostly) start modeling a set of game playing commands

--- ZHGameShared/ZHBonjourTCP.swift
+++ ZHGameShared/ZHBonjourTCP.swift
@@ -9,3 +9,22 @@
 import Foundation
 
 let ZHBonjourTCPServiceName = "_vaporwarewolf_service._tcp."
+
+
+
+enum ZHBonjourTCPCommandType: NSString {
+    case JoinGame = "join_game"
+    case LeaveGame = "leave_game"
+    case StartGame = "start_game"
+    case RoundQuestion = "round_question"
+    case RoundAnswer = "round_answer"
+    case GameOver = "game_over"
+    
+
+}
+
+class ZHBonjourTCPCommand: NSObject {
+    class func command(type: ZHBonjourTCPCommandType, payload: NSDictionary) -> NSDictionary {
+        return ["command": type.rawValue, "payload": payload]
+    }
+}
--- ZHGameTVOS/ViewController.swift
+++ ZHGameTVOS/ViewController.swift
@@ -33,8 +33,6 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
 extension ViewController: UICollectionViewDataSource {
